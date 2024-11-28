6bd8fcf43a1ec6e88fd6bd5d0c89c52621d3e586
hugoritimo <hugoritimo@gmail.com>
2024-11-28 14:31:06 -0300

    Mudanças

--- frontend/src/app/form/page.tsx
+++ frontend/src/app/form/page.tsx
@@ -5,7 +5,6 @@ import Button from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { Checkbox } from "@/components/ui/checkbox";
 import { motion } from "framer-motion";
 
 // Definição de Interfaces
