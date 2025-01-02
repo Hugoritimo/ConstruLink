b97b8cc438920709996410fd141aa6929b5b2e24
hugoritimo <hugoritimo@gmail.com>
2025-01-02 16:57:30 -0300

    Main

--- frontend/src/components/ButtoGenerateRDO.tsx
+++ frontend/src/components/ButtoGenerateRDO.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 const ButtonGenerateRDO: React.FC = () => {
   return <Button onClick={() => alert("RDO Generated!")}>Generate RDO</Button>;
 };
