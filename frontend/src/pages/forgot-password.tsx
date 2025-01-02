87e37325b05ff517f7e35af6a1d18606704f4f45
hugoritimo <hugoritimo@gmail.com>
2025-01-02 17:02:00 -0300

    Main

--- frontend/src/pages/forgot-password.tsx
+++ frontend/src/pages/forgot-password.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import Button from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ToastContainer, toast } from "react-toastify";
